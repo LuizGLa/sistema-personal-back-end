78776dd8d45baffb870f78df64822fdfb8e043a7
luiz.lima <luizgustavolima200@gmail.com>
2024-03-27 11:51:54 -0300

    Importação de modulo aluno

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsuarioModule } from './modules/usuarios/usuario.module';
 import * as ConfigEnv from '@nestjs/config';
+import { AlunoModule } from './modules/alunos/aluno.module';
 
 @Module({
   imports: [
     ConfigEnv.ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     AuthModule,
     UsuarioModule,
+    AlunoModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
